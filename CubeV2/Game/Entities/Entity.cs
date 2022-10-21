92db9a95868e13ef56adccf21a01d1d813d83ffb
simononeill493 <simononeill493@gmail.com>
2022-10-21 16:57:38 +0100

    disabled metavariables

--- CubeV2/Game/Entities/Entity.cs
+++ CubeV2/Game/Entities/Entity.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.Specialized;
 
 namespace CubeV2
@@ -36,7 +37,14 @@ namespace CubeV2
                 {
                     if (currentInstruction.OutputTargets[i] >= 0)
                     {
-                        Variables[currentInstruction.OutputTargets[i]] = currentInstruction.Outputs[i];
+                        var output = currentInstruction.Outputs[i];
+                        if(output.DefaultType==IVariableType.StoredVariable)
+                        {
+                            //We can't store variable references in variables yet. causes overflow. too meta lol
+                            continue;
+                        }
+
+                        Variables[currentInstruction.OutputTargets[i]] = output;
                     }
                 }
             }
